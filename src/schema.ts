3b12f2db977d622de4110afb4d38d392159805ee
projecteinf <projecteinf@boscdelacoma.cat>
2024-04-12 09:57:11 -0600

    Realització de consultes

--- src/schema.ts
+++ src/schema.ts
@@ -12,17 +12,36 @@ const typeDefinitions =  `
     description: String!
     url: String!
   } 
-`
 
   
-const resolvers = {
-  Query: {
-    hello: () => 'Hello World!'
-  }
+`
+
+type Link = {
+  id: string
+  url: string
+  description: string
 }
 
-
-
+const links: Link[] = [
+  {
+    id: 'link-0',
+    url: 'https://graphql-yoga.com',
+    description: 'The easiest way of setting up a GraphQL server'
+  }
+]
+ 
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!',
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url
+  }
+}
 
 export const schema = makeExecutableSchema({
   resolvers: [resolvers],
