0cf2cf762ca6b0b26f4b4dc0724a03c63c2bc6a6
projecteinf <projecteinf@boscdelacoma.cat>
2024-04-12 09:40:11 -0600

    create a simple GraphQL HTTP server on port 4000

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
-import { execute, parse } from 'graphql'
+import { createServer } from 'http'
+import { createYoga } from 'graphql-yoga'
 import { schema } from './schema'
  
-async function main() {
-  const myQuery = parse(`
-    query {
-      hello
-    }
-  `)
- 
-  const result = await execute({
-    schema,
-    document: myQuery
+function main() {
+  const yoga = createYoga({ schema })
+  const server = createServer(yoga)
+  server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql')
   })
- 
-  console.log(result)
 }
  
 main()
-
