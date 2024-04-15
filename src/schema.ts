ae3dc0068ebfe8e323552504e4bd1794a62872ed
projecteinf <projecteinf@boscdelacoma.cat>
2024-04-15 05:21:36 -0600

    Múltiples feeds

--- src/schema.ts
+++ src/schema.ts
@@ -27,6 +27,11 @@ const links: Link[] = [
     id: 'link-0',
     url: 'https://graphql-yoga.com',
     description: 'The easiest way of setting up a GraphQL server'
+  },
+  {
+    id: 'link-0',
+    url: 'https://graphql-yoga.com',
+    description: 'The easiest way of setting up a GraphQL server'
   }
 ]
  
